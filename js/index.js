c9fd46804745908f3210b30d92adae92b347873a
João Pedro Alves <jotalmeida007@hotmail.com>
2020-07-23 15:33:56 -0300

    Otimizando o código

--- js/index.js
+++ js/index.js
@@ -4,45 +4,34 @@ var body;
 
 body = document.querySelector('.body');
 
-
-    function pickColor(){
-        var $redRange = document.querySelector('#red'),
-            $redValue = document.querySelector('#redSpan');
-
-        $redRange.addEventListener('input', function() {
-        $redValue.textContent = this.value;
-        body.style.backgroundColor = `rgb(${$redRange.value},${$greenRange.value}, ${$blueRange.value})`;
-   
-        });
-
-        var $greenRange = document.querySelector('#green'),
-            $greenValue = document.querySelector('#greenSpan');
-
-        $greenRange.addEventListener('input', function() {
-        $greenValue.textContent = this.value;
-        body.style.backgroundColor = `rgb(${$redRange.value},${$greenRange.value}, ${$blueRange.value})`;
-        });
-
-        var $blueRange = document.querySelector('#blue'),
-            $blueValue = document.querySelector('#blueSpan');
-
-        $blueRange.addEventListener('input', function() {
-        $blueValue.textContent = this.value;
-        body.style.backgroundColor = `rgb(${$redRange.value},${$greenRange.value}, ${$blueRange.value})`;
-        });
-        
-        
+function pickColor() {
+  var $redRange = document.querySelector('#red'),
+    $redValue = document.querySelector('#redSpan');
 
 
 
+    
+  $redRange.addEventListener('input', function () {
+    $redValue.textContent = this.value;
+    body.style.backgroundColor = `rgb(${$redRange.value},${$greenRange.value}, ${$blueRange.value})`;
+  });
 
-            
-            
-        
-        }
+  var $greenRange = document.querySelector('#green'),
+    $greenValue = document.querySelector('#greenSpan');
 
-  
+  $greenRange.addEventListener('input', function () {
+    $greenValue.textContent = this.value;
+    body.style.backgroundColor = `rgb(${$redRange.value},${$greenRange.value}, ${$blueRange.value})`;
+  });
 
+  var $blueRange = document.querySelector('#blue'),
+    $blueValue = document.querySelector('#blueSpan');
+
+  $blueRange.addEventListener('input', function () {
+    $blueValue.textContent = this.value;
+    body.style.backgroundColor = `rgb(${$redRange.value},${$greenRange.value}, ${$blueRange.value})`;
+  });
+}
 
 //armazenar em variáveis
 
